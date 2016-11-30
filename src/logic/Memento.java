ca51abd826b3d280b603c0207bab2f7d6d9e3d31
gabrieljvenegasc <gabrieljvenegasc@gmail.com>
2016-11-29 18:26:58 -0600

    up1

--- src/logic/Memento.java
+++ src/logic/Memento.java
@@ -4,14 +4,11 @@ package logic;
 import java.util.*;
 
 public class Memento {
-
-    public Memento() {
-    }
-
     private List<Object> templateState;
 
-    public void Memento() {
-        // TODO implement here
+    public Memento(ArrayList<Object> pTemplateState) {
+        this.templateState=pTemplateState;
+    
     }
 
     public List<Object> getState() {
